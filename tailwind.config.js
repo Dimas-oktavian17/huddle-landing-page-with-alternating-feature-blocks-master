67e0bf46b427aad724ad36e1162dab2f558dc86a
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2022-06-12 13:00:14 +0700

    done all

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,17 @@ module.exports = {
       'poppins-sg': ['Poppins', 'sans-serif']
     },
     extend: { 
-      extend: {
+      padding:{
+        'hero-p':'0 0 16rem 0',
+        '15':'4.5rem'
+      },
       colors: {
         'pink-sg':'#ff52bf',
         'very-pale-cyan-sg':'#ebfbff',
         'very-dark-cyan-sg':'#00252e',
         'grayish-blue-sg':'#808d99',
       },
-    }},
+    },
   },
   plugins: [require("daisyui")],
     daisyui: {
